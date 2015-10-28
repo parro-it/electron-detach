bf21f543216f6d36e0bd3233a86cb6a7d590c0cf
Andrea Parodi <andrea@parro.it>
2015-10-28 21:45:59 +0100

    changed flag name

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
+'use strict';
+
 const app = require('app');
 const argv = require('yargs').argv;
 const spawn = require('child_process').spawn;
 
-module.exports = function electronDetach(opts = {}) {
-  if (argv.noDetach) {
+module.exports = function electronDetach(opts) {
+  opts = opts || {}; //eslint-disable-line
+  if (argv.detachedProcess) {
     // this is already a detached process, exit
     return;
   }
-
   if (!opts.requireCmdlineArg || argv.detach) {
     const args = process.argv
-        .slice(1)
-        .filter(a => a !== '--detach')
-        .concat(['--no-detach']);
+      .slice(1)
+      .filter(a => a !== '--detach')
+      .concat(['--detached-process']);
 
     const child = spawn('electron', args, { detached: true });
     child.unref();
