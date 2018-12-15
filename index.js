f23f94d75074ee2a291af0addd1833d1591e8044
Andrea Parodi <andrea@parro.it>
2018-12-15 14:12:58 +0100

    switch to xo

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 'use strict';
 
-const argv = require('yargs').argv;
-const spawn = require('child_process').spawn;
+const {spawn} = require('child_process');
 const fs = require('fs');
-const app = require('electron').app;
+const {app} = require('electron');
 const tty = require('tty');
+const {argv} = require('yargs');
 
-module.exports = function electronDetach(opts) {
-  opts = opts || {}; //eslint-disable-line
-  opts.outputPath = opts.outputPath || '/dev/null';
+function electronDetach(opts) {
+	opts = opts || {};
+	opts.outputPath = opts.outputPath || '/dev/null';
 
-  if (argv.detachedProcess || !tty.isatty(process.stdout.fd)) {
-    // this is already a detached process, exit
-    return true;
-  }
+	if (argv.detachedProcess || !tty.isatty(process.stdout.fd)) {
+		// This is already a detached process, exit
+		return true;
+	}
 
-  if (opts.requireCmdlineArg && !argv.detach) {
-    // process has to be restarted only if detach arg
-    // is specified on command line, and it isn't
-    return true;
-  }
+	if (opts.requireCmdlineArg && !argv.detach) {
+		// Process has to be restarted only if detach arg
+		// is specified on command line, and it isn't
+		return true;
+	}
 
-  const args = process.argv
-    .slice(1)
-    .filter(a => a !== '--detach')
-    .concat(['--detached-process']);
+	const args = process.argv
+		.slice(1)
+		.filter(a => a !== '--detach')
+		.concat(['--detached-process']);
 
-  const out = fs.openSync(opts.outputPath, 'a');
-  const err = fs.openSync(opts.outputPath, 'a');
+	const out = fs.openSync(opts.outputPath, 'a');
+	const err = fs.openSync(opts.outputPath, 'a');
 
-  const child = spawn(process.argv[0], args, {
-    detached: true,
-    stdio: [ 'ignore', out, err ]
-  });
-  child.unref();
-  app.quit();
-};
+	const child = spawn(process.argv[0], args, {
+		detached: true,
+		stdio: ['ignore', out, err]
+	});
+	child.unref();
+	app.quit();
+}
+
+module.exports = electronDetach;
--- test.js
+++ test.js
@@ -1,21 +1,20 @@
 'use strict';
 
 const electron = require('electron');
+const electronDetach = require('.');
 
 function start() {
-  process.stdout.write('We are writing to log file now.\n');
-  electron.app.on('ready', () => {
-    const win = new electron.BrowserWindow({});
-    win.loadURL('about://blank');
-  });
+	process.stdout.write('We are writing to log file now.\n');
+	electron.app.on('ready', () => {
+		const win = new electron.BrowserWindow({});
+		win.loadURL('about://blank');
+	});
 }
 
-const electronDetach = require('.');
 const appDetachedFromTerminal = electronDetach({
-  requireCmdlineArg: true,
-  // outputPath: __dirname + '/test.log'
+	requireCmdlineArg: true
 });
-
+console.log({appDetachedFromTerminal});
 if (appDetachedFromTerminal) {
-  start();
+	start();
 }
